bdc093a292e3704068b136869a9e4d04295f703b
Mike Gehard <mike.gehard@gmail.com>
2020-04-15 15:58:06 -0600

    Android message from component

--- applications/android/src/main/java/io/github/mikegehard/stufffriendssayandroid/MainActivity.kt
+++ applications/android/src/main/java/io/github/mikegehard/stufffriendssayandroid/MainActivity.kt
@@ -2,11 +2,16 @@ package io.github.mikegehard.stufffriendssay.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import dev.rubbersidedowntech.stufffriendssay.messages.hello
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView = findViewById<TextView>(R.id.helloText)
+        textView.text = hello()
     }
 }
